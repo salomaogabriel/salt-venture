50799f8b12b4f573435f1ab06bd6ca55b29929fd
salomaogabriel <65493468+salomaogabriel@users.noreply.github.com>
2022-07-18 10:10:36 +0200

    Add error message to sign up and focus border

--- client/src/Components/SignUp.tsx
+++ client/src/Components/SignUp.tsx
@@ -3,7 +3,7 @@ import '../Styles/SignUp.css';
 import { MdEmail, MdLock } from "react-icons/md";
 import { FaApple, FaUserAlt } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
-import { AiOutlineArrowLeft } from "react-icons/ai";
+import { BsArrowLeft } from "react-icons/bs";
 import { useState } from 'react';
 
 function SignUp() {
@@ -11,28 +11,33 @@ function SignUp() {
     return (
         <div className="sign-up">
             <div className="sign-up__header">
-                <AiOutlineArrowLeft className='sign-up__back-btn' />
+                <BsArrowLeft className='sign-up__back-btn' />
                 <h2>Sign up</h2>
             </div>
             <h1 className='create-account'>Create<br />Account</h1>
             <form>
                 <label htmlFor="email" className='sign-up__label'>Email
-                    <div className="input-wrapper">
+                    <div className="input-wrapper error__input">
                         <MdEmail />
                         <input type="text" name='email' id='email' placeholder='example@appliedtechnology.se' />
                     </div>
+                    <p className='error__msg'>Error Message</p>
                 </label>
                 <label htmlFor="username" className='sign-up__label'>Username
                     <div className="input-wrapper">
                         <FaUserAlt />
                         <input type="text" name='username' id='username' placeholder='username1234' />
                     </div>
+                    <p className='error__msg'></p>
+
                 </label>
                 <label htmlFor="password" className='sign-up__label'>Password
                     <div className="input-wrapper">
                         <MdLock />
                         <input type="password" name='password' id='password' placeholder='Password' />
                     </div>
+                    <p className='error__msg'></p>
+
                 </label>
                 <button className='sign-up__button'>Sign Up</button>
             </form>
