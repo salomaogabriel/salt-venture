1ec1492de5a8a7ad8910bb0dfd341c573063cc7a
NicholasIzosimov <104497770+NicholasIzosimov@users.noreply.github.com>
2022-07-26 11:29:38 +0200

    Edit hangman Repo

--- SaltVenture.API/Services/Hangman/HangmanRepository.cs
+++ SaltVenture.API/Services/Hangman/HangmanRepository.cs
@@ -39,35 +39,6 @@ public class HangmanRepository : IHangmanRepository
         return await _context.HangmanGames.FirstOrDefaultAsync(g => g.Id == gameId);
     }
 
-    public async Task<HangmanWordRequest> GetWord(HangmanWordRequest word)
-    {
-            //Define your baseUrl
-            string baseUrl = "https://random-words-api.vercel.app/word";
-            //Have your using statements within a try/catch block
-            try
-            {
-                //We will now define your HttpClient with your first using statement which will use a IDisposable.
-                using (HttpClient client = new HttpClient())
-                {
-                    //In the next using statement you will initiate the Get Request, use the await keyword so it will execute the using statement in order.
-                    //The HttpResponseMessage which contains status code, and data from response.
-                    using (var res = await client.GetAsync(baseUrl))
-                    {
-                        Console.WriteLine(res);
-                        return JsonConvert.DeserializeObject<HangmanWordRequest>(res.Content.ToString()!);
-                        //Then get the data or content from the response in the next using statement, then within it you will get the data, and convert it to a c# object.
-                    }
-                }
-            } catch(Exception exception)
-            {
-                Console.WriteLine("Exception Hit------------");
-                Console.WriteLine(exception);
-            throw new ArgumentException("Failed to fetch");
-
-            }
-            throw new ArgumentException("Failed to fetch");
-    }
-
     public async Task<Hangman> UpdateGame(Hangman game)
     {
            var updateGame = _context.HangmanGames?.Update(game);
