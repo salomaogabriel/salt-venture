58ed87b87ac33e6f7c709c5385c9c18a9762cb1c
salomaogabriel <65493468+salomaogabriel@users.noreply.github.com>
2022-07-26 16:07:16 +0200

    Only look for fraudulent if the user is active

--- client/src/Components/Admin.tsx
+++ client/src/Components/Admin.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import '../Styles/admin.css'
 import { Bar } from 'react-chartjs-2';
 import {
     Chart as ChartJS,
@@ -10,6 +10,9 @@ import {
     Tooltip,
     Legend,
 } from 'chart.js';
+import { Link } from 'react-router-dom';
+import { TbSalt } from 'react-icons/tb';
+import { FaUserAlt } from 'react-icons/fa';
 
 ChartJS.register(
     CategoryScale,
@@ -34,9 +37,45 @@ interface Props {
 function Admin({ user, logout }: Props) {
 
     const [data, setData] = useState<any>([]);
-    const [labels,setLabels] = useState<any>([]);
+    const [labels, setLabels] = useState<any>([]);
     const [chartData, setChartData] = useState<any>([]);
     const [isLoading, setIsLoading] = useState(true);
+    const deleteUser = async(id) => {
+        const requestSettings = {
+            method: 'DELETE',
+            headers: {
+                'Authorization': "Bearer " + user.token,
+                "Content-Type": "application/json"
+            }
+        };
+        try {
+            const response = await fetch("https://saltventure.azurewebsites.net/api/admin/delete/"+id
+            
+            , requestSettings)
+            if (!response.ok) {
+                throw new Error(JSON.stringify(await response.json()));
+            }
+            const deserializedJSON = await response.json();
+
+            // deserializedJSON.usersMoneyRange
+            let tempData = [];
+            let templabels = [];
+            for (const [key, value] of Object.entries(deserializedJSON.usersMoneyRange)) {
+                if (key == "$id" || parseInt(key) <= 0) continue;
+                templabels.push(key);
+                tempData.push(value);
+            }
+
+            setIsLoading(false);
+            setLabels(templabels)
+            setChartData(tempData);
+            setData(deserializedJSON);
+            console.log(deserializedJSON);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
     const getUsers = async () => {
         setIsLoading(true);
         if (user.id == undefined) return;
@@ -54,15 +93,15 @@ function Admin({ user, logout }: Props) {
                 throw new Error(JSON.stringify(await response.json()));
             }
             const deserializedJSON = await response.json();
-            
+
             // deserializedJSON.usersMoneyRange
             let tempData = [];
             let templabels = [];
             for (const [key, value] of Object.entries(deserializedJSON.usersMoneyRange)) {
-                if(key == "$id" || parseInt(key) <= 0 ) continue;
+                if (key == "$id" || parseInt(key) <= 0) continue;
                 templabels.push(key);
                 tempData.push(value);
-              }
+            }
 
             setIsLoading(false);
             setLabels(templabels)
@@ -91,19 +130,40 @@ function Admin({ user, logout }: Props) {
             }
         ]
     }
-    
+
     return (
         <div className='admin-page'>
             <button onClick={logout}>Log out</button>
             <div className="admin-info-row">
                 <h2>Money Made: {data.houseWinnings}</h2>
+                <h2>Bets Made: {data.betsMade}</h2>
                 <h2>Total Users: {data.usersNo}</h2>
                 <h2>Active: {data.activeUsersNo}</h2>
             </div>
             <div className="admin-info-graph">
                 <Bar data={dataChart} />
             </div>
-            {data.possibleFraudulentUSers ? <></> : <></>}
+            <div className="admin-fraudulent-list">
+                {data.possibleFraudulentUSers ? <>
+                    {data.possibleFraudulentUSers["$values"].map((e) => {
+                        return (<> <div className='admin-list-item'>
+                            <div className="admin-list-item-info">
+                            <FaUserAlt className='rank-list-item-img' />
+                            <div className="rank-list-item-info">
+                                <p className='rank-list-item__username'>{e.username}</p>
+
+                                <p className='rank-list-item__balance'><TbSalt className='salt-shaker' /> {e.balance}</p>
+                            </div>
+                            </div>
+                            <div className="admin-list-item-buttons">
+                            <button className='admin-list-item-button'>View</button>
+                            <button className='admin-list-item-buttons--delete'>Delete</button>
+                            </div>
+                        </div>
+                            <hr className='rank-divider' /></>);
+                    })}
+                </> : <>No users found!</>}
+            </div>
         </div>
     );
 }
